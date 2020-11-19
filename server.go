5505a84c8b3cfe8e7bb8891bbbb2605043c2aa4f
David Li <davidli012345@gmail.com>
2020-11-19 19:53:06 +0000

    adding semicolon to PORT

--- server.go
+++ server.go
@@ -1,29 +1,30 @@
 package main
 
-import ( 
-  "github.com/gofiber/fiber/v2"
-  "github.com/FriendlyUser/finfiber/pkg/discord"
-  "github.com/FriendlyUser/finfiber/pkg/finance"
-  "github.com/FriendlyUser/finfiber/pkg/nlp"
-  "github.com/FriendlyUser/finfiber/pkg/twitter"
-  "os"
+import (
+	"os"
+
+	"github.com/FriendlyUser/finfiber/pkg/discord"
+	"github.com/FriendlyUser/finfiber/pkg/finance"
+	"github.com/FriendlyUser/finfiber/pkg/nlp"
+	"github.com/FriendlyUser/finfiber/pkg/twitter"
+	"github.com/gofiber/fiber/v2"
 )
 
 func getenv(key, fallback string) string {
-  value := os.Getenv(key)
-  if len(value) == 0 {
-      return fallback
-  }
-  return value
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return ":" + value
 }
 
 // add function to check env vars before running
 func main() {
-  app := fiber.New()
-  port := getenv("PORT", "8080")
-  app.Get("/api/v1/tickers", finance.GetTickersPandas)
-  app.Get("/api/v1/sentiment", nlp.GetSentiment)
-  app.Get("/api/v1/twitter", twitter.GetTwitterSimple)
-  app.Post("/api/v1/discord", discord.SendDiscordHook)
-  app.Listen(port)
+	app := fiber.New()
+	port := getenv("PORT", ":8080")
+	app.Get("/api/v1/tickers", finance.GetTickersPandas)
+	app.Get("/api/v1/sentiment", nlp.GetSentiment)
+	app.Get("/api/v1/twitter", twitter.GetTwitterSimple)
+	app.Post("/api/v1/discord", discord.SendDiscordHook)
+	app.Listen(port)
 }
