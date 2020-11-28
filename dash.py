867f6c7bcc517496302d8500a1bdcd62815e35c0
dli <davidli012345@gmail.com>
2020-11-28 09:54:21 -0800

    parameterizing project name

--- dash.py
+++ dash.py
@@ -9,7 +9,6 @@ import plotly.express as px
 # -------------------------- PYTHON FUNCTIONS ---------------------------- #
 
 
-
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(
@@ -22,26 +21,27 @@ app.config.suppress_callback_exceptions = True
 
 # -------------------------- PROJECT DASHBOARD ---------------------------- #
 
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = pd.DataFrame(
+    {
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+    }
+)
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
+app.layout = html.Div(
+    children=[
+        html.H1(children="Hello Dash"),
+        html.Div(
+            children="""
         Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
+    """
+        ),
+        dcc.Graph(id="example-graph", figure=fig),
+    ]
+)
 
 
 # -------------------------- MAIN ---------------------------- #
