96c59a8c2588b7a11ca0ccdef00085538f37ee72
dli <davidli012345@gmail.com>
2020-11-28 19:07:53 -0800

    Updating youtube api functionality

--- python/api/pyfastapi.py
+++ python/api/pyfastapi.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Query
+from ytube_dl import get_video
 
 app = FastAPI()
 
@@ -8,3 +10,20 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hello TutLinks.com"}
+
+
+@app.get("/video")
+def video(
+    q: Optional[str] = Query(
+        None,
+        max_length=50,
+        title="Youtube Video Query",
+        description="Query Params - should have youtube id",
+    )
+):
+    if q:
+        print(q)
+        video_id = q.get("id")
+        video_data = get_video(video_id)
+        return {"q": q, "video_data": video_data}
+    return {"error": "no id in query parameter"}
